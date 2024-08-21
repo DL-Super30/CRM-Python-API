d9539495f651b1ff06834b735c9f25f2c33d7ecb
GundlapalliMamatha <mamatha.rachha@gmail.com>
2024-08-21 18:13:23 +0530

    added authtoken in settings and imported in login app

--- skill-capital-crm/login/models.py
+++ skill-capital-crm/login/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
