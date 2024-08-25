79996fb5663c97a6e905afa3a7f6ea24b85dcf8d
GundlapalliMamatha <mamatha.rachha@gmail.com>
2024-08-25 22:55:15 +0530

    added auth,permission classes

--- skill-capital-crm/CRM/settings.py
+++ skill-capital-crm/CRM/settings.py
@@ -49,6 +49,16 @@ INSTALLED_APPS = [
     'getleads'
 ]
 
+# Authentication settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
