278310da236fd22c0bc8a15d9ff45aa4bbb4387d
Jayanth60 <vallabhaneni221@gmail.com>
2024-09-02 14:48:33 +0530

    CRUD on opportunities

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ from fastapi.security import OAuth2PasswordRequestForm
 SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_hex(32))
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 1
-REMEMBER_ME_EXPIRE_DAYS = 1
 
 app = FastAPI(docs_url=None)
 
@@ -99,6 +98,71 @@ class getLead(BaseModel):
     next_followup: datetime
     created_at: datetime
 
+class Opportunity(BaseModel):
+    name: str
+    cc : str
+    phone: str
+    email: str
+    fee_quoted: str
+    batch_timing: str
+    lead_status: str
+    stack: str
+    class_mode: str
+    description: str
+    oppo_status: str
+    oppo_stage: str
+    Demo_stage: str
+    visited_stage: str
+    lost_reason: str
+    next_followup: datetime
+    lead_source: str
+    course: str
+
+class getOpportunity(BaseModel):
+    id:str
+    name: str
+    cc : str
+    phone: str
+    email: str
+    fee_quoted: str
+    batch_timing: str
+    lead_status: str
+    stack: str
+    class_mode: str
+    description: str
+    oppo_status: str
+    oppo_stage: str
+    Demo_stage: str
+    visited_stage: str
+    lost_reason: str
+    next_followup: datetime
+    lead_source: str
+    course: str
+    created_at:datetime
+
+# Any Table Existence
+def check_table_exists(schema, table_name):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        
+        query = sql.SQL(
+            "SELECT EXISTS ("
+            "SELECT FROM information_schema.tables "
+            "WHERE table_schema = %s AND table_name = %s);"
+        )
+        
+        cur.execute(query, (schema, table_name))
+        exists = cur.fetchone()[0]
+        cur.close()
+        conn.close()
+       
+        return exists
+    except (Exception, psycopg2.Error) as error:
+        print(f"Error checking table existence: {error}")
+        return False
+
+
 #Inserting client.
 @app.post("/insert_client/")
 async def insert_client(client: Client):
@@ -160,26 +224,6 @@ async def insert_client(client: Client):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-def check_table_exists(schema, table_name):
-    try:
-        conn = get_db_connection()
-        cur = conn.cursor()
-        
-        query = sql.SQL(
-            "SELECT EXISTS ("
-            "SELECT FROM information_schema.tables "
-            "WHERE table_schema = %s AND table_name = %s);"
-        )
-        
-        cur.execute(query, (schema, table_name))
-        exists = cur.fetchone()[0]
-        cur.close()
-        conn.close()
-       
-        return exists
-    except (Exception, psycopg2.Error) as error:
-        print(f"Error checking table existence: {error}")
-        return False
     
 # Access token
 def create_access_token(data: dict, expires_delta: timedelta):
@@ -197,7 +241,6 @@ async def check_client(request: Request, form_data: OAuth2PasswordRequestForm = 
         conn = get_db_connection()
         cur = conn.cursor()
 
-        
         query = sql.SQL('''
             SELECT password FROM public.clients WHERE email = %s
         ''')
@@ -254,7 +297,7 @@ async def read_users_me(current_user: dict = Security(get_current_user)):
 
 
 
-#Leads code
+# Leads code
 @app.post("/createleads")
 async def insert_lead(lead: Lead):
     try:
@@ -296,8 +339,7 @@ async def insert_lead(lead: Lead):
             lead.lead_status , lead.lead_source , lead.stack , lead.course , lead.class_mode , lead.next_followup,
             created_at
         )
-        
-
+    
         cur.execute(insert_query,values)
         conn.commit()
         cur.close()
@@ -307,27 +349,6 @@ async def insert_lead(lead: Lead):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-def check_table_exists(schema, table_name):
-    try:
-        conn = get_db_connection()
-        cur = conn.cursor()
-        
-        query = sql.SQL(
-            "SELECT EXISTS ("
-            "SELECT FROM information_schema.tables "
-            "WHERE table_schema = %s AND table_name = %s);"
-        )
-        
-        cur.execute(query, (schema, table_name))
-        exists = cur.fetchone()[0]
-        cur.close()
-        conn.close()
-       
-        return exists
-    except (Exception, psycopg2.Error) as error:
-        logging.error(f"Error checking table existence: {error}")
-        return False
 
 
 # Getting leads.
@@ -376,7 +397,6 @@ async def get_leads():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-
 #update lead
 @app.put("/updatelead/{lead_id}")
 async def update_lead(lead_id: str, lead: Lead):
@@ -387,7 +407,6 @@ async def update_lead(lead_id: str, lead: Lead):
         if not check_table_exists("public", "leads"):
             raise HTTPException(status_code=404, detail=str('No data to update'))
         
-
         query = sql.SQL('''
             SELECT id FROM public.leads WHERE id = %s
         ''')
@@ -466,6 +485,225 @@ async def delete_lead(lead_id: str):
         conn.close()
 
         return {"message": f"Lead {name} deleted successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# Create Opportunities
+@app.post("/createopportunity")
+async def insert_opportunity(opportunity: Opportunity):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        if not check_table_exists("public", "opportunities"):
+            create_table_query = sql.SQL('''
+                CREATE TABLE public.opportunities (
+                    id UUID PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL,
+                    cc VARCHAR(255) NOT NULL,
+                    phone VARCHAR(20) NOT NULL,
+                    email VARCHAR(255) NOT NULL,
+                    fee_quoted VARCHAR(50) NOT NULL,
+                    batch_timing VARCHAR(50) NOT NULL,
+                    lead_status VARCHAR(50) NOT NULL,
+                    stack VARCHAR(50) NOT NULL,
+                    class_mode VARCHAR(50) NOT NULL,
+                    description TEXT,
+                    oppo_status VARCHAR(50) NOT NULL,
+                    oppo_stage VARCHAR(50) NOT NULL,
+                    Demo_stage VARCHAR(50) NOT NULL,
+                    visited_stage VARCHAR(50) NOT NULL,
+                    lost_reason VARCHAR(255),
+                    next_followup TIMESTAMP NOT NULL,
+                    lead_source VARCHAR(50) NOT NULL,
+                    course VARCHAR(50) NOT NULL,
+                    created_at TIMESTAMP NOT NULL
+                );
+            ''')
+            cur.execute(create_table_query)
+            conn.commit()
+
+        insert_query = sql.SQL('''
+            INSERT INTO public.opportunities (id, name, cc, phone, email, fee_quoted, batch_timing, description, 
+            lead_status, stack, class_mode, oppo_status, oppo_stage, Demo_stage, visited_stage, lost_reason, 
+            next_followup, lead_source, course, created_at)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ''')
+        
+        oppo_id = str(uuid.uuid4())
+        created_at = datetime.now(timezone.utc)
+
+        values = (
+            oppo_id, opportunity.name, opportunity.cc, opportunity.phone, opportunity.email, opportunity.fee_quoted, 
+            opportunity.batch_timing, opportunity.description, opportunity.lead_status, opportunity.stack, 
+            opportunity.class_mode, opportunity.oppo_status, opportunity.oppo_stage, opportunity.Demo_stage, 
+            opportunity.visited_stage, opportunity.lost_reason, opportunity.next_followup, opportunity.lead_source, 
+            opportunity.course, created_at
+        )
+    
+        cur.execute(insert_query, values)
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return {"message": f"Opportunity {opportunity.name} added successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+
+# Getting Opportunities
+@app.get("/getOpportunities", response_model=List[getOpportunity])
+async def get_opportunities():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        if not check_table_exists("public", "opportunities"):
+            raise HTTPException(status_code=404, detail='No data to display')
+
+        select_query = sql.SQL('''
+            SELECT id, name, cc, phone, email, fee_quoted, batch_timing, description, 
+                   lead_status, lead_source, stack, course, class_mode, oppo_status, oppo_stage, 
+                   Demo_stage, visited_stage, lost_reason, next_followup, created_at
+            FROM public.opportunities;
+        ''')
+
+        cur.execute(select_query)
+        rows = cur.fetchall()
+        cur.close()
+        conn.close()
+
+        opportunities = []
+        for row in rows:
+            opportunity = getOpportunity(
+                id=row[0],
+                name=row[1],
+                cc=row[2],
+                phone=row[3],
+                email=row[4],
+                fee_quoted=row[5],
+                batch_timing=row[6],
+                description=row[7],
+                lead_status=row[8],
+                lead_source=row[9],
+                stack=row[10],
+                course=row[11],
+                class_mode=row[12],
+                oppo_status=row[13],
+                oppo_stage=row[14],
+                Demo_stage=row[15],
+                visited_stage=row[16],
+                lost_reason=row[17],
+                next_followup=row[18],
+                created_at=row[19]
+            )
+            opportunities.append(opportunity)
+
+        return opportunities
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+# Update Opportunities
+@app.put("/updateopportunity/{opportunity_id}")
+async def update_opportunity(opportunity_id: str, opportunity: Opportunity):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        if not check_table_exists("public", "opportunities"):
+            raise HTTPException(status_code=404, detail='No data to update')
+        
+        query = sql.SQL('''
+            SELECT id FROM public.opportunities WHERE id = %s
+        ''')
+        cur.execute(query, (opportunity_id,))
+        existing_opportunity = cur.fetchone()
+
+        if not existing_opportunity:
+            raise HTTPException(status_code=404, detail="Opportunity not found")
+
+        update_query = sql.SQL('''
+            UPDATE public.opportunities
+            SET name = %s,
+                cc = %s,
+                phone = %s,
+                email = %s,
+                fee_quoted = %s,
+                batch_timing = %s,
+                description = %s,
+                lead_status = %s,
+                stack = %s,
+                class_mode = %s,
+                oppo_status = %s,
+                oppo_stage = %s,
+                Demo_stage = %s,
+                visited_stage = %s,
+                lost_reason = %s,
+                next_followup = %s,
+                lead_source = %s,
+                course = %s,
+                created_at = %s
+            WHERE id = %s
+        ''')
+
+        created_at = datetime.now(timezone.utc)
+
+        updated_values = (
+            opportunity.name, opportunity.cc, opportunity.phone, opportunity.email, opportunity.fee_quoted,
+            opportunity.batch_timing, opportunity.description, opportunity.lead_status, opportunity.stack,
+            opportunity.class_mode, opportunity.oppo_status, opportunity.oppo_stage, opportunity.Demo_stage,
+            opportunity.visited_stage, opportunity.lost_reason, opportunity.next_followup, opportunity.lead_source,
+            opportunity.course, created_at, opportunity_id
+        )
+
+        cur.execute(update_query, updated_values)
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return {"message": f"Opportunity {opportunity.name} updated successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+# Delete Opportunities
+@app.delete("/deleteopportunity/{opportunity_id}")
+async def delete_opportunity(opportunity_id: str):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        if not check_table_exists("public", "opportunities"):
+            raise HTTPException(status_code=404, detail='No data to delete')
+
+        query = sql.SQL('''
+            SELECT id, name FROM public.opportunities WHERE id = %s
+        ''')
+        cur.execute(query, (opportunity_id,))
+        existing_opportunity = cur.fetchone()
+
+        if not existing_opportunity:
+            raise HTTPException(status_code=404, detail="Opportunity not found with that id")
+        
+        oppo_id, name = existing_opportunity
+
+        delete_query = sql.SQL('''
+            DELETE FROM public.opportunities WHERE id = %s
+        ''')
+        cur.execute(delete_query, (oppo_id,))
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+        return {"message": f"Opportunity {name} deleted successfully"}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
